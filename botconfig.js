188bfdf05951ec8f8968a044a97a261277b7e924
raffii3424235 <79677667+raffii3424235@users.noreply.github.com>
2021-08-28 09:40:17 +0700

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,45 @@
+module.exports = {
+  Admins: ["UserID", "UserID"], //Admins of the bot
+  ExpressServer: true,//If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+  Token: process.env.Token || "ODc4ODA1MTg3MTQ4NTM3OTI2.YSGhHA.2JoX9XWYbC-0ICwRK-LftQAWQCQ", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "878805187148537926", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "kung", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+
+  //Lavalink
+  /*
+   Lavalink: {
+    id: "Main",
+    host: "lava.link",
+    port: 80,
+    pass: "youshallnotpass", 
+    secure: false, // Set this to true if you're self-hosting lavalink on replit.
+  },
+  */
+  //Alternate Lavalink
+  
+  Lavalink: {
+    id: "Main",
+    host: "lava.sudhan.tech",
+    port: 1234,
+    pass: "CodingWithSudhan", 
+    secure: false // Set this to true if you're self-hosting lavalink on replit.
+  },
+
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "d005dcc96c204e40b33cd65fb6fa03ae", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+  },
+};
